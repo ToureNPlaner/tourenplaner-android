de4ebee0e7a376e16eb35872357d9da83912536b
Steffen Hanikel <hanikesn@studi.informatik.uni-stuttgart.de>
2011-09-14 14:06:56 +0200

    ToureNPlaner Projekt hinzugefügt

---
+++ ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/ToureNPlanerActivity.java
@@ -0,0 +1,13 @@
+package de.uni.stuttgart.informatik.ToureNPlaner;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ToureNPlanerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
