360b285b0acfbf137fb542ae26177bb978bf226e
Steffen Hanikel <steffen.hanikel@googlemail.com>
2011-10-07 21:26:22 +0200

    Simplified MapScreen

--- ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/UI/Activities/MapScreen.java
+++ ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/UI/Activities/MapScreen.java
@@ -1,250 +1,199 @@
 package de.uni.stuttgart.informatik.ToureNPlaner.UI.Activities;
 
-import java.util.List;
-import java.util.Vector;
-import org.mapsforge.android.maps.ArrayWayOverlay;
-import org.mapsforge.android.maps.GeoPoint;
-import org.mapsforge.android.maps.MapActivity;
-import org.mapsforge.android.maps.MapView;
-import org.mapsforge.android.maps.MapViewMode;
-import org.mapsforge.android.maps.Overlay;
-import org.mapsforge.android.maps.OverlayItem;
-import org.mapsforge.android.maps.OverlayWay;
-
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.DashPathEffect;
-import android.graphics.Paint;
+import android.graphics.*;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.View.OnTouchListener;
-import de.uni.stuttgart.informatik.ToureNPlaner.R;
 import de.uni.stuttgart.informatik.ToureNPlaner.Data.Node;
 import de.uni.stuttgart.informatik.ToureNPlaner.Data.NodeModel;
 import de.uni.stuttgart.informatik.ToureNPlaner.Data.SessionData;
-import de.uni.stuttgart.informatik.ToureNPlaner.UI.Overlays.MapGestureDetectorOverlay;
-import de.uni.stuttgart.informatik.ToureNPlaner.UI.Overlays.NodeOverlay;
-import de.uni.stuttgart.informatik.ToureNPlaner.UI.Overlays.Listener.OverlayListener;
+import de.uni.stuttgart.informatik.ToureNPlaner.R;
+import org.mapsforge.android.maps.*;
+
+import java.util.Vector;
 
 public class MapScreen extends MapActivity {
-	public static MapScreen mapScreenContext;
-	public static MapView mapView;
-	private static NodeOverlay itemizedoverlay;
-	private static List<Overlay> mapOverlays;
-	private MapGestureDetectorOverlay mapGestureOverlay;
-	private static Drawable iconStart;
-	private static Drawable iconNormal;
-	private static Drawable iconEnd;
-	private static Vector<OverlayItem> overlayItemVector = new Vector<OverlayItem>();
-	private static ArrayWayOverlay wayOverlay;
+    public MapView mapView;
+    private ArrayItemizedOverlay itemizedoverlay;
+    private Drawable iconStart;
+    private Drawable iconNormal;
+    private Drawable iconEnd;
+    private Vector<OverlayItem> overlayItemVector = new Vector<OverlayItem>();
+    private ArrayWayOverlay wayOverlay;
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
-        SessionData.Instance.save(outState);
         super.onSaveInstanceState(outState);
     }
 
     @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        SessionData.Instance.load(savedInstanceState);
-    }
-
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.mapscreen);
-		// setting properties of the mapview
-		mapView = new MapView(this);
-		mapView.setClickable(true);
-		mapView.setLongClickable(true);
-		mapView.setBuiltInZoomControls(true);
-		mapView.setMapViewMode(MapViewMode.MAPNIK_TILE_DOWNLOAD);
-		// mapView.setMapFile("/sdcard/berlin.map");
-		mapView.setFpsCounter(true);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // setting properties of the mapview
+        mapView = new MapView(this);
+        mapView.setClickable(true);
+        mapView.setLongClickable(true);
+        mapView.setBuiltInZoomControls(true);
+        mapView.setMapViewMode(MapViewMode.MAPNIK_TILE_DOWNLOAD);
+        // mapView.setMapFile("/sdcard/berlin.map");
+        mapView.setFpsCounter(true);
         mapView.setMemoryCardCachePersistence(true);
         mapView.setMemoryCardCacheSize(1000);
-		setContentView(mapView);
+        setContentView(mapView);
 
-		mapOverlays = mapView.getOverlays();
+        setupIcons();
 
-		// initialize the Icons and ItemOverlays
+        itemizedoverlay = new ArrayItemizedOverlay(iconNormal, this);
+        mapView.getOverlays().add(itemizedoverlay);
 
-		iconStart = this.getResources().getDrawable(R.drawable.startmarker);
-		iconStart.setBounds(0, 0, iconStart.getIntrinsicWidth(),
-				iconStart.getIntrinsicHeight());
+        // On TouchListener for mapView handles events and call the
+        // MapGestureOverlay
+        mapView.getOverlays().add(new Overlay() {
+            @Override
+            public boolean onLongPress(GeoPoint geoPoint, MapView mapView) {
+                String markerName = "Marker Nr. "
+                        + String.valueOf(NodeModel.getInstance().size() + 1);
+                Node node = new Node(markerName, geoPoint);
+                NodeModel.getInstance().addNodeToVector(node);
+                addMarkerToMap(node);
+                return true;
+            }
 
-		iconNormal = this.getResources().getDrawable(R.drawable.marker);
-		iconNormal.setBounds(0, 0, iconNormal.getIntrinsicWidth(),
-				iconNormal.getIntrinsicHeight());
+            @Override
+            protected void drawOverlayBitmap(Canvas canvas, Point drawPosition, Projection projection, byte drawZoomLevel) {
 
-		iconEnd = this.getResources().getDrawable(R.drawable.endmarker);
-		iconEnd.setBounds(0, 0, iconEnd.getIntrinsicWidth(),
-				iconEnd.getIntrinsicHeight());
+            }
+        });
+        printAllMarkersToMap();
 
-		itemizedoverlay = new NodeOverlay(iconNormal, this);
 
-		// initialize components of MapGestureOverlay
-		mapGestureOverlay = new MapGestureDetectorOverlay(new OverlayListener());
-		mapGestureOverlay.setIsLongpressEnabled(true);
-		mapOverlays.add(mapGestureOverlay);
+        setupWayOverlay();
 
-		// On TouchListener for mapView handles events and call the
-		// MapGestureOverlay
-		mapView.setOnTouchListener(new OnTouchListener() {
-			@Override
-			public boolean onTouch(View v, MotionEvent event) {
-				mapGestureOverlay.onTouchEvent(event, mapView);
-				return false;
-			}
-		});
-		printAllMarkersToMap();
-		// TEMP
 
-		GeoPoint geoPoint1 = new GeoPoint(52.514446, 13.350150); // Berlin
-																	// Victory
-																	// Column
-//		GeoPoint geoPoint2 = new GeoPoint(52.516272, 13.377722); // Brandenburg
-//																	// Gate
-//		GeoPoint geoPoint3 = new GeoPoint(52.525, 13.369444); // Berlin Central
-//																// Station
-//		GeoPoint geoPoint4 = new GeoPoint(52.52, 13.369444); // German
-//																// Chancellery
-//		GeoPoint geoPoint5 = new GeoPoint(52516999, 13388900);
+        // set focus of MapScreen
+        GeoPoint geoPoint1 = new GeoPoint(52.514446, 13.350150); // Berlin
+        mapView.getController().setCenter(geoPoint1);
+    }
 
-		//
-		// addMarkerToMap(geoPoint1, "1", "HH");
-		// addMarkerToMap(geoPoint2, "2", "HH");
-		// addMarkerToMap(geoPoint3, "3", "HH");
-		// addMarkerToMap(geoPoint4, "4", "HH");
-		// addMarkerToMap(geoPoint5, "5", "HH");
-		//
+    private void setupIcons() {
+        // initialize the Icons and ItemOverlays
+        iconStart = this.getResources().getDrawable(R.drawable.startmarker);
+        iconStart.setBounds(0, 0, iconStart.getIntrinsicWidth(),
+                iconStart.getIntrinsicHeight());
 
-		// END TEMP
+        iconNormal = this.getResources().getDrawable(R.drawable.marker);
+        iconNormal.setBounds(0, 0, iconNormal.getIntrinsicWidth(),
+                iconNormal.getIntrinsicHeight());
 
-		// ----------------WayOverlay Properties-----------------
-		// create the default paint objects for overlay ways
-		Paint wayDefaultPaintFill = new Paint(Paint.ANTI_ALIAS_FLAG);
-		wayDefaultPaintFill.setStyle(Paint.Style.STROKE);
-		wayDefaultPaintFill.setColor(Color.BLUE);
-		wayDefaultPaintFill.setAlpha(160);
-		wayDefaultPaintFill.setStrokeWidth(7);
-		wayDefaultPaintFill.setStrokeJoin(Paint.Join.ROUND);
-		wayDefaultPaintFill.setPathEffect(new DashPathEffect(new float[] { 20,
-				20 }, 0));
-		Paint wayDefaultPaintOutline = new Paint(Paint.ANTI_ALIAS_FLAG);
-		wayDefaultPaintOutline.setStyle(Paint.Style.STROKE);
-		wayDefaultPaintOutline.setColor(Color.BLUE);
-		wayDefaultPaintOutline.setAlpha(128);
-		wayDefaultPaintOutline.setStrokeWidth(7);
-		wayDefaultPaintOutline.setStrokeJoin(Paint.Join.ROUND);
+        iconEnd = this.getResources().getDrawable(R.drawable.endmarker);
+        iconEnd.setBounds(0, 0, iconEnd.getIntrinsicWidth(),
+                iconEnd.getIntrinsicHeight());
+    }
 
-		// create an individual paint object for an overlay way
-		Paint wayPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-		wayPaint.setStyle(Paint.Style.FILL);
-		wayPaint.setColor(Color.YELLOW);
-		wayPaint.setAlpha(192);
-		// create the WayOverlay and add the ways
-		wayOverlay = new ArrayWayOverlay(wayDefaultPaintFill,
-				wayDefaultPaintOutline);
+    private void setupWayOverlay() {
+        // ----------------WayOverlay Properties-----------------
+        // create the default paint objects for overlay ways
+        Paint wayDefaultPaintFill = new Paint(Paint.ANTI_ALIAS_FLAG);
+        wayDefaultPaintFill.setStyle(Paint.Style.STROKE);
+        wayDefaultPaintFill.setColor(Color.BLUE);
+        wayDefaultPaintFill.setAlpha(160);
+        wayDefaultPaintFill.setStrokeWidth(7);
+        wayDefaultPaintFill.setStrokeJoin(Paint.Join.ROUND);
+        wayDefaultPaintFill.setPathEffect(new DashPathEffect(new float[]{20,
+                20}, 0));
+        Paint wayDefaultPaintOutline = new Paint(Paint.ANTI_ALIAS_FLAG);
+        wayDefaultPaintOutline.setStyle(Paint.Style.STROKE);
+        wayDefaultPaintOutline.setColor(Color.BLUE);
+        wayDefaultPaintOutline.setAlpha(128);
+        wayDefaultPaintOutline.setStrokeWidth(7);
+        wayDefaultPaintOutline.setStrokeJoin(Paint.Join.ROUND);
 
-		mapView.getOverlays().add(wayOverlay);
+        // create an individual paint object for an overlay way
+        Paint wayPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        wayPaint.setStyle(Paint.Style.FILL);
+        wayPaint.setColor(Color.YELLOW);
+        wayPaint.setAlpha(192);
+        // create the WayOverlay and add the ways
+        wayOverlay = new ArrayWayOverlay(wayDefaultPaintFill,
+                wayDefaultPaintOutline);
+        mapView.getOverlays().add(wayOverlay);
+    }
 
-		// set focus of MapScreen
-		mapView.getController().setCenter(geoPoint1);
-	}
+    // ----------------Menu-----------------
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mapscreenmenu, menu);
+        return true;
+    }
 
-	// ----------------Menu-----------------
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.mapscreenmenu, menu);
-		return true;
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.calculate:
+                // generates an intent from the class NodeListScreen
+                Intent myIntent = new Intent(this, NodelistScreen.class);
+                startActivity(myIntent);
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle item selection
-		switch (item.getItemId()) {
-		case R.id.calculate:
-			// generates an intent from the class NodeListScreen
-			Intent myIntent = new Intent(this, NodelistScreen.class);
-			startActivity(myIntent);
+                return true;
+            case R.id.reset:
+                addPathToMap();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
-			return true;
-		case R.id.reset:
-			addPathToMap();
-			return true;
-		default:
-			return super.onOptionsItemSelected(item);
-		}
-	}
+    public void addMarkerToMap(Node node) {
+        OverlayItem overlayitem = new OverlayItem(node.getGeoPoint(),
+                node.getName(), "");
+        // overlayitem.setMarker(iconNormal);
+        overlayItemVector.add(overlayitem);
+        setMarkerIconsToNodes();
+        itemizedoverlay.addItem(overlayitem);
+    }
 
-	// ---------------------------------------
+    public void addPathToMap() {
+        wayOverlay.clear();
+        OverlayWay way;
+        GeoPoint[][] geoArray = new GeoPoint[1][NodeModel.getInstance().size()];
+        for (int i = 0; i < NodeModel.getInstance().size(); i++) {
+            geoArray[0][i] = NodeModel.getInstance().get(i).getGeoPoint();
+        }
+        way = new OverlayWay(geoArray);
+        wayOverlay.addWay(way);
+    }
 
-	/**
-	 * this methode adds a marker to the mapview ( on the itemmapoverlay)
-	 * 
-	 * @param gp
-	 *            is a GeoPoint
-	 * @param title
-	 *            of an item
-	 * @param message
-	 *            of an item
-	 */
-	public static void addMarkerToMap(Node node) {
-		OverlayItem overlayitem = new OverlayItem(node.getGeoPoint(),
-				node.getName(), "");
-		// overlayitem.setMarker(iconNormal);
-		overlayItemVector.add(overlayitem);
-		setMarkerIconsToNodes();
-		itemizedoverlay.addOverlay(overlayitem);
+    private void setMarkerIconsToNodes() {
+        //TODO fix Bug EndIcon changes position when transformed into normalIcon
+        if (SessionData.Instance.getAlgorithmHasStarAndEndMarker()) {
+            for (int i = 0; i < overlayItemVector.size(); i++) {
+                if (i == 0) {
+                    overlayItemVector.get(i).setMarker(iconStart);
+                } else if (i == overlayItemVector.size() - 1) {
+                    overlayItemVector.get(i).setMarker(iconEnd);
+                } else {
+                    overlayItemVector.get(i).setMarker(iconNormal);
+                }
+            }
 
-		mapOverlays.add(itemizedoverlay);
-	}
+        }
+    }
 
-	public static void addPathToMap() {
-		wayOverlay.clear();
-		OverlayWay way;
-		GeoPoint[][] geoArray = new GeoPoint[1][NodeModel.getInstance().size()];
-		for (int i = 0; i < NodeModel.getInstance().size(); i++) {
-			geoArray[0][i] = NodeModel.getInstance().get(i).getGeoPoint();
-		}
-		way = new OverlayWay(geoArray);
-		wayOverlay.addWay(way);
-	}
-
-	private static void setMarkerIconsToNodes() {
-		//TODO fix Bug EndIcon changes position when transformed into normalIcon 
-		if (SessionData.Instance.getAlgorithmHasStarAndEndMarker()) {
-			for (int i = 0; i < overlayItemVector.size(); i++) {
-				if (i == 0) {
-					overlayItemVector.get(i).setMarker(iconStart);
-				} else if (i == overlayItemVector.size() - 1) {
-					overlayItemVector.get(i).setMarker(iconEnd);
-				} else {
-					overlayItemVector.get(i).setMarker(iconNormal);
-				}
-			}
-
-		}
-	}
-
-	public static void printAllMarkersToMap() {
-		// initialize components of ItemOverlay
-		overlayItemVector.clear();
-		//TODO update view when pressing "backbutton" on "NodeListScreen" Activitiy
-		Log.v("clearMap","ClearMap");
-		for (int i = 0; i < NodeModel.getInstance().size(); i++) {
-			Log.v("add marker","add marker");
-			addMarkerToMap(NodeModel.getInstance().get(i));
-		}
-	}
+    public void printAllMarkersToMap() {
+        // initialize components of ItemOverlay
+        overlayItemVector.clear();
+        //TODO update view when pressing "backbutton" on "NodeListScreen" Activitiy
+        Log.v("clearMap", "ClearMap");
+        for (int i = 0; i < NodeModel.getInstance().size(); i++) {
+            Log.v("add marker", "add marker");
+            addMarkerToMap(NodeModel.getInstance().get(i));
+        }
+    }
 
 }
