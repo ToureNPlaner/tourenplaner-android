e124190b7953372d044fc5ac7980f4016d644ca0
Steffen Hanikel <steffen.hanikel@googlemail.com>
2011-10-07 21:26:41 +0200

    small fixes in Nodelist

--- ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/UI/Activities/NodelistScreen.java
+++ ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/UI/Activities/NodelistScreen.java
@@ -1,7 +1,5 @@
 package de.uni.stuttgart.informatik.ToureNPlaner.UI.Activities;
 
-import java.util.Vector;
-
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,13 +9,20 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
+import de.uni.stuttgart.informatik.ToureNPlaner.Data.Adapters.NodeListAdapter;
 import de.uni.stuttgart.informatik.ToureNPlaner.Data.Node;
 import de.uni.stuttgart.informatik.ToureNPlaner.Data.NodeModel;
 import de.uni.stuttgart.informatik.ToureNPlaner.Data.SessionData;
-import de.uni.stuttgart.informatik.ToureNPlaner.Data.Adapters.NodeListAdapter;
+
+import java.util.Vector;
 
 public class NodelistScreen extends ListActivity {
 	public NodelistScreen nodeListScreenContext = this;
+    private final MapScreen mapScreen;
+
+    public NodelistScreen(MapScreen mapScreen) {
+        this.mapScreen = mapScreen;
+    }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
@@ -30,9 +35,8 @@ public class NodelistScreen extends ListActivity {
         super.onRestoreInstanceState(savedInstanceState);
         SessionData.Instance.load(savedInstanceState);
     }
-	
-	private ListView listView;
-	private static NodeListAdapter adapter;
+
+    private static NodeListAdapter adapter;
 		public static  NodeListAdapter getAdapter() {
 		return adapter;
 	}
@@ -47,27 +51,27 @@ public class NodelistScreen extends ListActivity {
 		
 		Vector<Node> nodeList = NodeModel.getInstance().getNodeVector();
 		adapter = new NodeListAdapter(nodeList, this);
-		listView = getListView();
+            ListView listView = getListView();
 		listView.setAdapter(adapter);
 		listView.setOnItemClickListener(new OnItemClickListener() {
 
-			public void onItemClick(AdapterView<?> adapter, View view,
-					final int pos, long arg3) {
-				SessionData.Instance.setSelectedNode(pos);
-				// generates an intent from the class NodeListScreen
-				Intent myIntent = new Intent(nodeListScreenContext,
-						NodePreferences.class);
-				startActivity(myIntent);
-				getAdapter().notifyDataSetChanged();
-				}
-	});
+            public void onItemClick(AdapterView<?> adapter, View view,
+                                    final int pos, long arg3) {
+                SessionData.Instance.setSelectedNode(pos);
+                // generates an intent from the class NodeListScreen
+                Intent myIntent = new Intent(nodeListScreenContext,
+                        NodePreferences.class);
+                startActivity(myIntent);
+                getAdapter().notifyDataSetChanged();
+            }
+        });
 	}
 		
 		@Override
 		public boolean onKeyDown(int keyCode, KeyEvent event) {
 		    if ((keyCode == KeyEvent.KEYCODE_BACK)) {
 		        Log.v("back button pressed","back button pressed");
-		        MapScreen.printAllMarkersToMap();
+		        mapScreen.printAllMarkersToMap();
 				}
 		    return super.onKeyDown(keyCode, event);
 		}
@@ -75,4 +79,4 @@ public class NodelistScreen extends ListActivity {
 		public static void getNodePreferences(Integer pos){
 		
 		}
-};
+}
