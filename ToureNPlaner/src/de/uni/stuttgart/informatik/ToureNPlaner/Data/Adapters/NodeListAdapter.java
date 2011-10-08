8d3f20f5c908734718e60d21d18fbe8ae5bf7552
Steffen Hanikel <steffen.hanikel@googlemail.com>
2011-10-08 17:39:37 +0200

    Simplified NodeListAdapter

--- ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/Data/Adapters/NodeListAdapter.java
+++ ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/Data/Adapters/NodeListAdapter.java
@@ -5,7 +5,7 @@ import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -14,60 +14,37 @@ import de.uni.stuttgart.informatik.ToureNPlaner.R;
 
 import java.util.ArrayList;
 
-public class NodeListAdapter extends BaseAdapter{
-	  private  ArrayList<Node> nodeVector;
-	  private Context context;
-
+public class NodeListAdapter extends ArrayAdapter<Node> {
     public NodeListAdapter(ArrayList<Node> nodeVector, Context context) {
-
-		 this.nodeVector = nodeVector;
-		 this.context = context;
+        super(context, android.R.layout.simple_list_item_1, nodeVector);
     }
-	
-	 @Override
-   public View getView(final int position, View convertView, ViewGroup parent) {
-		  LinearLayout itemLayout;
-	        Node node = nodeVector.get(position);
-	 
-	        itemLayout= (LinearLayout) LayoutInflater.from(context).inflate(R.layout.nodelistadapteritem, parent, false);
-	 
-	        TextView tvUser = (TextView) itemLayout.findViewById(R.id.toptext);
-	        tvUser.setText(node.getName());
-	 
-	        TextView tvText = (TextView) itemLayout.findViewById(R.id.bottomtext);
-	        tvText.setText(node.getGeoPoint().toString());
-	        
-	        ImageView ImageView = (ImageView) itemLayout.findViewById(R.id.icon);
-	        Drawable icon = context.getResources().getDrawable(R.drawable.icon);
-	     
-	        
-	        // sets the icon depending on the index
-	        if (position == 0){
-	        	icon =  context.getResources().getDrawable(R.drawable.startmarker);
-	        }else if(position ==nodeVector.size()-1 ){
-	        	icon =  context.getResources().getDrawable(R.drawable.endmarker);
-	        }else{
-	        	icon =  context.getResources().getDrawable(R.drawable.marker);
-	        	        }
-	        ImageView.setImageDrawable(icon);
-	        return itemLayout;
-   }
 
-	@Override
-	public int getCount() {
-			return nodeVector.size();
-	}
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        LinearLayout itemLayout;
 
-	@Override
-	public Object getItem(int position) {
-			return nodeVector.get(position);
-	}
+        Node node = getItem(position);
 
-	@Override
-	public long getItemId(int position) {
-			return 0;
-	}
+        itemLayout = (LinearLayout) LayoutInflater.from(getContext()).inflate(R.layout.nodelistadapteritem, parent, false);
 
-	
+        TextView tvUser = (TextView) itemLayout.findViewById(R.id.toptext);
+        tvUser.setText(node.getName());
 
+        TextView tvText = (TextView) itemLayout.findViewById(R.id.bottomtext);
+        tvText.setText(node.getGeoPoint().toString());
+
+        ImageView ImageView = (ImageView) itemLayout.findViewById(R.id.icon);
+        Drawable icon;
+
+        // sets the icon depending on the index
+        if (position == 0) {
+            icon = getContext().getResources().getDrawable(R.drawable.startmarker);
+        } else if (position == getCount() - 1) {
+            icon = getContext().getResources().getDrawable(R.drawable.endmarker);
+        } else {
+            icon = getContext().getResources().getDrawable(R.drawable.marker);
+        }
+        ImageView.setImageDrawable(icon);
+        return itemLayout;
+    }
 }
