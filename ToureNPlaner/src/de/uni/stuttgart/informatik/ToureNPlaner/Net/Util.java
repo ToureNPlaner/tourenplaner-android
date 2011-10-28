831a293fc3965c7a50a0283637fee86b1211444a
Steffen Hanikel <steffen.hanikel@googlemail.com>
2011-10-28 11:24:55 +0200

    Removed dead code

--- ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/Net/Util.java
+++ ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/Net/Util.java
@@ -3,17 +3,6 @@ package de.uni.stuttgart.informatik.ToureNPlaner.Net;
 import java.io.*;
 
 public class Util {
-    /*static String streamToString(InputStream stream) throws Exception {
-        BufferedReader in = new BufferedReader(new InputStreamReader(stream));
-        StringBuilder stringBuilder = new StringBuilder();
-        String line;
-        while ((line = in.readLine()) != null) {
-            stringBuilder.append(line);
-        }
-        return stringBuilder.toString();
-    } */
-
-
     static String streamToString(InputStream stream) {
         Writer writer = new StringWriter();
         char[] buffer = new char[1024];
