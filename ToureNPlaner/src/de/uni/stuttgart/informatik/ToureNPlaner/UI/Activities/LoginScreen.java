8abb936c094c2e7d53c233e364bdb2f892da6677
Vivian Eggert <Vivian.Eggert@gmx.net>
2011-12-26 12:46:04 +0100

    added save credentials in loginscreen

--- ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/UI/Activities/LoginScreen.java
+++ ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/UI/Activities/LoginScreen.java
@@ -1,22 +1,29 @@
 package de.uni.stuttgart.informatik.ToureNPlaner.UI.Activities;
 
+import java.util.prefs.Preferences;
+
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 import de.uni.stuttgart.informatik.ToureNPlaner.Net.*;
 import de.uni.stuttgart.informatik.ToureNPlaner.R;
 import de.uni.stuttgart.informatik.ToureNPlaner.UI.Dialogs.MyProgressDialog;
+import de.uni.stuttgart.informatik.ToureNPlaner.Util.Base64;
 
 public class LoginScreen extends FragmentActivity implements Observer {
 	private AuthRequestHandler handler;
     private Session session;
-
+	public static final String SHARED_PREFERENCES_CREDENTIALS = "credentials";
+	SharedPreferences.Editor preferencesEditor;
+    CheckBox chk_saveCredentials;
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         outState.putSerializable(Session.IDENTIFIER, session);
@@ -41,12 +48,21 @@ public class LoginScreen extends FragmentActivity implements Observer {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loginscreen);
-
+        
+        SharedPreferences applicationPreferences = getSharedPreferences(
+        		SHARED_PREFERENCES_CREDENTIALS, MODE_PRIVATE);
+        preferencesEditor = applicationPreferences.edit();
+        
+        
+        chk_saveCredentials = (CheckBox) findViewById(R.id.chkb_login_screen_save_credentials);
         EditText emailTextfield = (EditText) findViewById(R.id.emailTextfield);
-        emailTextfield.setText("root@toureNPlaner.de");
+        
+        // TODO reset credentials with empty strings 
+        emailTextfield.setText(applicationPreferences.getString("user","root@toureNPlaner.de"));
         EditText passwordTextfield = (EditText) findViewById(R.id.passwordTextfield);
-        passwordTextfield.setText("toureNPlaner");
-
+        passwordTextfield.setText(Base64.decodeString(applicationPreferences.getString("password", Base64.encodeString("toureNPlaner"))));
+        chk_saveCredentials.setChecked(applicationPreferences.getBoolean("chk_isChecked",false));
+        
         // If we get created for the first time we get our data from the intent
         if (savedInstanceState != null) {
             session = (Session) savedInstanceState.getSerializable(Session.IDENTIFIER);
@@ -72,7 +88,16 @@ public class LoginScreen extends FragmentActivity implements Observer {
                 // set userdata
                 session.setUsername(emailTextfield.getText().toString());
                 session.setPassword(passwordTextfield.getText().toString());
-
+              
+                if(chk_saveCredentials.isChecked()){
+                preferencesEditor.putString("user", emailTextfield.getText().toString());
+                preferencesEditor.putString("password", Base64.encodeString(passwordTextfield.getText().toString()));
+                preferencesEditor.putBoolean("chk_isChecked", true);
+                preferencesEditor.commit();
+                }else{
+                	preferencesEditor.clear();
+                    preferencesEditor.commit();
+                }
 	            ConnectionProgressDialog.newInstance("Login", session.getUsername()).show(getSupportFragmentManager(), "login");
                 handler = new AuthRequestHandler(LoginScreen.this, session);
 	            handler.execute();
