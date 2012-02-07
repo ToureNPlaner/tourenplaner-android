dc19c8bbb803c1db7a111234db489f6620ad0d6e
Steffen Hanikel <steffen.hanikel@googlemail.com>
2012-02-07 19:11:37 +0100

    Error handlinge implementiert

--- ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/Net/Handler/RawHandler.java
+++ ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/Net/Handler/RawHandler.java
@@ -2,10 +2,14 @@ package de.uni.stuttgart.informatik.ToureNPlaner.Net.Handler;
 
 import android.os.AsyncTask;
 import android.util.Log;
+import de.uni.stuttgart.informatik.ToureNPlaner.Data.Error;
 import de.uni.stuttgart.informatik.ToureNPlaner.Net.DoneHandlerInputStream;
 import de.uni.stuttgart.informatik.ToureNPlaner.Net.JacksonManager;
 import de.uni.stuttgart.informatik.ToureNPlaner.Net.Observer;
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 
@@ -46,8 +50,20 @@ public abstract class RawHandler extends AsyncTask<Void, Void, Object> {
 			HttpURLConnection urlConnection = getHttpUrlConnection();
 
 			try {
-				InputStream stream = new DoneHandlerInputStream(urlConnection.getInputStream());
-				return handleInput(JacksonManager.ContentType.parse(urlConnection.getContentType()), stream);
+				InputStream stream;
+				try {
+					stream = new DoneHandlerInputStream(urlConnection.getInputStream());
+				} catch (IOException exception) {
+					stream = new DoneHandlerInputStream(urlConnection.getErrorStream());
+				}
+				JacksonManager.ContentType type = JacksonManager.ContentType.parse(urlConnection.getContentType());
+
+				if (urlConnection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+					ObjectMapper mapper = JacksonManager.getMapper(type);
+					throw Error.parse(mapper.readValue(stream, JsonNode.class));
+				}
+
+				return handleInput(type, stream);
 			} finally {
 				urlConnection.disconnect();
 			}
