41eb5f99cbcb132c91292be4d843fd29d5cc2d9c
Steffen Hanikel <steffen.hanikel@googlemail.com>
2012-02-07 21:30:31 +0100

    Save state correctly in EditNodeScreen

--- ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/UI/Activities/EditNodeScreen.java
+++ ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/UI/Activities/EditNodeScreen.java
@@ -1,87 +1,74 @@
 package de.uni.stuttgart.informatik.ToureNPlaner.UI.Activities;
 
-import java.io.Serializable;
-
 import android.app.Activity;
-import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.TextView;
-import de.uni.stuttgart.informatik.ToureNPlaner.Data.AlgorithmInfo;
+import android.widget.*;
 import de.uni.stuttgart.informatik.ToureNPlaner.Data.Constraint;
 import de.uni.stuttgart.informatik.ToureNPlaner.Data.Node;
 import de.uni.stuttgart.informatik.ToureNPlaner.Net.Session;
-import de.uni.stuttgart.informatik.ToureNPlaner.UI.Adapters.ConstraintListAdapter;
-import de.uni.stuttgart.informatik.ToureNPlaner.UI.Adapters.NodeListAdapter;
 import de.uni.stuttgart.informatik.ToureNPlaner.R;
+import de.uni.stuttgart.informatik.ToureNPlaner.UI.Adapters.ConstraintListAdapter;
+
+import java.io.Serializable;
 
 public class EditNodeScreen extends Activity {
+	public static final int RESULT_DELETE = RESULT_FIRST_USER;
 
-    private Node node;
-    private Session session;
-    public static final int RESULT_DELETE = RESULT_FIRST_USER;
-	private Bundle data;
+	private Session session;
+	private Node node;
+	private int index;
+
 	private ConstraintListAdapter adapter;
-	 
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        outState.putSerializable(Session.IDENTIFIER, session);
-        super.onSaveInstanceState(outState);
-    }
-    void finishActivity() {
-        Intent data = new Intent();
-        data.putExtra("node", node);
-	    data.putExtra("index", this.data.getInt("index", 0));
-        setResult(RESULT_OK, data);
-        finish();
-    }
-    @Override
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		outState.putSerializable(Session.IDENTIFIER, session);
+		outState.putSerializable("node", node);
+		outState.putSerializable("index", index);
+
+		super.onSaveInstanceState(outState);
+	}
+
+	void finishActivity() {
+		Intent data = new Intent();
+		data.putExtra("node", node);
+		data.putExtra("index", index);
+		setResult(RESULT_OK, data);
+		finish();
+	}
+
+	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.nodepreferences);
+
 		// If we get created for the first time we get our data from the intent
-        if(savedInstanceState != null) {
-            session = (Session) savedInstanceState.getSerializable(Session.IDENTIFIER);
-        } else {
-            session = (Session) getIntent().getSerializableExtra(Session.IDENTIFIER);
-        }
-		try {
-            if (savedInstanceState != null) {
-	            data = savedInstanceState;
-                node = (Node) data.getSerializable("node");
-            } else {
-	            data = getIntent().getExtras();
-                node = (Node) data.getSerializable("node");
-            }
-		} catch (Exception e) {
-			e.printStackTrace();
+		Bundle data = savedInstanceState != null ? savedInstanceState : getIntent().getExtras();
+		session = (Session) data.getSerializable(Session.IDENTIFIER);
+		node = (Node) data.getSerializable("node");
+		index = data.getInt("index");
+
+		if (!session.getSelectedAlgorithm().getPointConstraints().isEmpty()) {
+			setupListView();
 		}
-        if(session.getSelectedAlgorithm().getPointConstraints().size() > 0){
-            	setupListView();
-        }
-        setupButtons();
-    }    
-    
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (resultCode) {
-            case RESULT_OK:
-                node.getConstraintList().set(data.getExtras().getInt("index"), (Constraint) data.getSerializableExtra("constraint"));
-                adapter.notifyDataSetChanged();
-                break;
-        }
-    }
-    
-    private void setupButtons(){
-    	// -------------- get EditTexts --------------
+		setupButtons();
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		switch (resultCode) {
+			case RESULT_OK:
+				node.getConstraintList().set(data.getExtras().getInt("index"), (Constraint) data.getSerializableExtra("constraint"));
+				adapter.notifyDataSetChanged();
+				break;
+		}
+	}
+
+	private void setupButtons() {
+		// -------------- get EditTexts --------------
 		final EditText etName = (EditText) findViewById(R.id.etName);
 		// -------------- get Buttons --------------
 		Button btnDelete = (Button) findViewById(R.id.btnDelete);
@@ -92,43 +79,44 @@ public class EditNodeScreen extends Activity {
 		btnSave.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				node.setName(etName.getText().toString());
-                finishActivity();
+				finishActivity();
 			}
 		});
 		btnReturn.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
-                setResult(RESULT_CANCELED, null);
-                finish();
+				setResult(RESULT_CANCELED, null);
+				finish();
 			}
 		});
 		// -----------------btnDelete-----------------------
 		btnDelete.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
-                setResult(RESULT_DELETE, new Intent().putExtra("index",data.getInt("index", 0)));
-                finish();
+				setResult(RESULT_DELETE, new Intent().putExtra("index", index));
+				finish();
 			}
 		});
 
-	
-    }
-    private void setupListView() {
- 
-        ListView listView = (ListView) findViewById(R.id.listViewNodePreferences);
-        TextView constraintLabel = (TextView) this.findViewById(R.id.lblNodePreferencesConstraintText);
-        // set title visible
-        constraintLabel.setVisibility(0);
-        adapter = new ConstraintListAdapter(node.getConstraintList(), this);
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent myIntent = new Intent(EditNodeScreen.this,
-                            EditConstraintScreen.class);
-                myIntent.putExtra("constraint", (Serializable) adapterView.getItemAtPosition(i));
-                myIntent.putExtra("index",i);
-                myIntent.putExtra(Session.IDENTIFIER,session);
-                startActivityForResult(myIntent,0);
-            }
-        });
-    }
+
+	}
+
+	private void setupListView() {
+
+		ListView listView = (ListView) findViewById(R.id.listViewNodePreferences);
+		TextView constraintLabel = (TextView) this.findViewById(R.id.lblNodePreferencesConstraintText);
+		// set title visible
+		constraintLabel.setVisibility(0);
+		adapter = new ConstraintListAdapter(node.getConstraintList(), this);
+		listView.setAdapter(adapter);
+		listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+				Intent myIntent = new Intent(EditNodeScreen.this,
+						EditConstraintScreen.class);
+				myIntent.putExtra("constraint", (Serializable) adapterView.getItemAtPosition(i));
+				myIntent.putExtra("index", i);
+				myIntent.putExtra(Session.IDENTIFIER, session);
+				startActivityForResult(myIntent, 0);
+			}
+		});
+	}
 }
