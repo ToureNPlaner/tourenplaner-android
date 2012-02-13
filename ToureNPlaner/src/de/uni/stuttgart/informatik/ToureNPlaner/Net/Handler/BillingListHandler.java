1c1bdbda464036abfe4f9feda225c92e08ad1f33
Vivian Eggert <Vivian.Eggert@gmx.net>
2012-02-13 16:36:08 +0100

    added first version of billingview with endlesslist (not working correctly yet)

--- ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/Net/Handler/BillingListHandler.java
+++ ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/Net/Handler/BillingListHandler.java
@@ -1,14 +1,10 @@
 package de.uni.stuttgart.informatik.ToureNPlaner.Net.Handler;
 
 import java.io.InputStream;
-import java.net.HttpURLConnection;
 import java.util.ArrayList;
-
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
-
 import de.uni.stuttgart.informatik.ToureNPlaner.Data.BillingItem;
-import de.uni.stuttgart.informatik.ToureNPlaner.Data.User;
 import de.uni.stuttgart.informatik.ToureNPlaner.Net.JacksonManager;
 import de.uni.stuttgart.informatik.ToureNPlaner.Net.Observer;
 import de.uni.stuttgart.informatik.ToureNPlaner.Net.Session;
@@ -16,15 +12,25 @@ import de.uni.stuttgart.informatik.ToureNPlaner.Net.JacksonManager.ContentType;
 import de.uni.stuttgart.informatik.ToureNPlaner.UI.Activities.BillingScreen;
 
 public class BillingListHandler extends ConnectionHandler{
-private ArrayList<BillingItem> billinglist;
-private String limitString = "limit=100";
+private String limitString = "limit=200";
 private String offsetString = "offset=0";
+private Integer offset = 0;
 	public BillingListHandler(Observer listener, Session session) {
 		super(listener, session);
 	
 	}
 	public void setLimit(int i){
-		limitString = "limit="+Integer.valueOf(i);
+		i+=50;
+//		if(i >= 150){
+//			limitString = "limit="+Integer.valueOf(i);
+//		
+//			offset +=75;
+//		setOffset(i-50);
+//		}
+			limitString = "limit="+Integer.valueOf(i);
+	}
+	public void setOffset(int i){
+		offsetString = "offset="+Integer.valueOf(i);
 	}
 	@Override
 	protected String getSuffix() {
@@ -38,7 +44,7 @@ private String offsetString = "offset=0";
 	@Override
 	protected Object handleInput(ContentType type, InputStream inputStream)throws Exception {
 		ObjectMapper mapper = JacksonManager.getMapper(type);
-		BillingScreen.billinglist.clear();
+
 		BillingScreen.billinglist = ((BillingItem.parse(mapper.readValue(inputStream, JsonNode.class))));
 	    return true;
 	}
