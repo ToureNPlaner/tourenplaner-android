98c229a627f44d245bd00c1ac5036af3806c413a
Steffen Hanikel <steffen.hanikel@googlemail.com>
2012-02-15 00:21:38 +0100

    Certificate Management

--- ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/UI/Activities/CertificateScreen.java
+++ ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/UI/Activities/CertificateScreen.java
@@ -1,36 +1,127 @@
 package de.uni.stuttgart.informatik.ToureNPlaner.UI.Activities;
 
-import de.uni.stuttgart.informatik.ToureNPlaner.R;
-import de.uni.stuttgart.informatik.ToureNPlaner.UI.Activities.ServerScreen.NewDialog;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+import de.uni.stuttgart.informatik.ToureNPlaner.R;
+import de.uni.stuttgart.informatik.ToureNPlaner.UI.Adapters.KeystoreAdapter;
+
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+import java.util.Collection;
+import java.util.List;
 
 public class CertificateScreen extends FragmentActivity {
+	private KeyStore keyStore;
+	private KeystoreAdapter adapter;
 
-	
+	private void addCert(Uri uri) {
+		try {
+			InputStream fileInputStream = getApplicationContext().getContentResolver().openInputStream(uri);
+			CertificateFactory factory = CertificateFactory.getInstance("X.509");
+			Collection<? extends Certificate> certs = factory.generateCertificates(fileInputStream);
+			List<String> segments = uri.getPathSegments();
+			int num = 0;
+			for (Certificate cert : certs) {
+				keyStore.setCertificateEntry(segments.get(segments.size() - 1) + num, cert);
+				num++;
+			}
+		} catch (Exception e) {
+			Toast.makeText(getBaseContext(), e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+		}
+		adapter.notifyDataSetChanged();
+		storeKeystore();
+	}
+
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.certificatescreen);
 
-		//loadServerList();
+		loadKeystore();
 
-		//setupListView();
-		//setupAddButton();
-
+		setupListView();
+		setupAddButton();
 	}
-	
+
+	private void storeKeystore() {
+		try {
+			FileOutputStream outputStream = openFileOutput("keystore", MODE_PRIVATE);
+			try {
+				keyStore.store(outputStream, null);
+			} finally {
+				outputStream.close();
+			}
+		} catch (Exception e) {
+			Toast.makeText(getBaseContext(), e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+		}
+	}
+
+	private void loadKeystore() {
+		try {
+			keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+		} catch (Exception e) {
+			Toast.makeText(getBaseContext(), e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+			return;
+		}
+		try {
+			keyStore.load(openFileInput("keystore"), null);
+			return;
+		} catch (Exception e) {
+			Toast.makeText(getBaseContext(), e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+		}
+
+		try {
+			keyStore.load(new KeyStore.LoadStoreParameter() {
+				@Override
+				public KeyStore.ProtectionParameter getProtectionParameter() {
+					return new KeyStore.PasswordProtection(null);
+				}
+			});
+		} catch (Exception e) {
+			Toast.makeText(getBaseContext(), e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+		}
+	}
+
+	private void setupListView() {
+		ListView listView = (ListView) findViewById(R.id.certificateListView);
+		adapter = new KeystoreAdapter(getApplicationContext(), keyStore);
+		listView.setAdapter(adapter);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (resultCode == RESULT_OK) {
+			addCert(data.getData());
+		}
+	}
+
 	private void setupAddButton() {
 		Button btnAdd = (Button) findViewById(R.id.btnAdd);
 		btnAdd.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				NewDialog.newInstance(getResources().getString(R.string.new_string), "")
-						.show(getSupportFragmentManager(), NewDialog.IDENTIFIER);
+				Intent intent = new Intent("org.openintents.action.PICK_FILE");
+				intent.setData(Uri.parse("file:///sdcard/"));
+				intent.putExtra("org.openintents.extra.TITLE", "Please select a file");
+				try {
+					startActivityForResult(intent, 1);
+				} catch (ActivityNotFoundException e) {
+					Toast.makeText(getApplicationContext(), e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+				}
 			}
 		});
 	}
 }
+
---
+++ ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/UI/Adapters/KeystoreAdapter.java
@@ -0,0 +1,72 @@
+package de.uni.stuttgart.informatik.ToureNPlaner.UI.Adapters;
+
+import android.content.Context;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.util.Enumeration;
+
+public class KeystoreAdapter extends BaseAdapter {
+	private final Context context;
+	private final KeyStore keyStore;
+
+	public KeystoreAdapter(Context context, KeyStore keyStore) {
+		this.context = context;
+		this.keyStore = keyStore;
+	}
+
+	@Override
+	public int getCount() {
+		try {
+			return keyStore.size();
+		} catch (KeyStoreException e) {
+			return 0;
+		}
+	}
+
+	@Override
+	public Object getItem(int i) {
+		try {
+			Enumeration<String> alias = keyStore.aliases();
+			while (i-- > 0) {
+				alias.nextElement();
+			}
+			return alias.nextElement();
+		} catch (KeyStoreException e) {
+			return null;
+		}
+	}
+
+	public TextView getGenericView() {
+		// Layout parameters for the ExpandableListView
+		AbsListView.LayoutParams lp = new AbsListView.LayoutParams(
+				ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+		TextView textView = new TextView(context);
+		textView.setLayoutParams(lp);
+		// Center the text vertically
+		textView.setGravity(Gravity.CENTER_VERTICAL | Gravity.LEFT);
+		// Set the text starting position
+		textView.setPadding(0, 0, 0, 0);
+		textView.setTextSize(20);
+		return textView;
+	}
+
+	@Override
+	public long getItemId(int i) {
+		return (long) i;
+	}
+
+	@Override
+	public View getView(int i, View view, ViewGroup viewGroup) {
+		TextView textView = getGenericView();
+		textView.setText(getItem(i).toString());
+		return textView;
+	}
+}
