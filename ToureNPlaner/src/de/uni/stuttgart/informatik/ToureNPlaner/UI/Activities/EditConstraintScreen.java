893c5f92a6041f3034ee9337651ebfe1aacce067
Steffen Hanikel <steffen.hanikel@googlemail.com>
2012-02-14 14:06:16 +0100

    Implemented functional EditConstraintScreen

--- ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/UI/Activities/EditConstraintScreen.java
+++ ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/UI/Activities/EditConstraintScreen.java
@@ -14,19 +14,21 @@ import de.uni.stuttgart.informatik.ToureNPlaner.UI.ConstraintViews.ConstraintVie
 
 public class EditConstraintScreen extends Activity {
 	private Constraint constraint;
-	private Bundle data;
+	private int index;
+
 	private ConstraintView constraintView;
 
 	@Override
 	protected void onSaveInstanceState(Bundle outState) {
-		outState.putAll(data);
+		outState.putInt("index", index);
+		outState.putSerializable("constraint", constraint);
 		super.onSaveInstanceState(outState);
 	}
 
 	void finishActivity() {
 		Intent data = new Intent();
 		data.putExtra("value", constraint.getValue());
-		data.putExtra("index", this.data.getInt("index", 0));
+		data.putExtra("index", index);
 		setResult(RESULT_OK, data);
 		finish();
 	}
@@ -35,12 +37,15 @@ public class EditConstraintScreen extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		// If we get created for the first time we get our data from the intent
-		data = savedInstanceState != null ? savedInstanceState : getIntent().getExtras();
+		Bundle data = savedInstanceState != null ? savedInstanceState : getIntent().getExtras();
 
 		constraint = (Constraint) data.getSerializable("constraint");
+		index = data.getInt("index", 0);
 
-		constraintView = constraint.createView(this);
+		setupView();
+	}
 
+	private void setupView() {
 		setContentView(R.layout.editconstraints);
 
 		final TextView etName = (TextView) findViewById(R.id.txtconstName);
@@ -58,83 +63,7 @@ public class EditConstraintScreen extends Activity {
 
 		final ViewGroup placeholder = (ViewGroup) findViewById(R.id.EditConstraintPlaceHolder);
 
-		getLayoutInflater().inflate(constraintView.getLayout(), placeholder);
-
-
-		/*//------------------get TextView ------------------
-		   TextView lblMin = (TextView) findViewById(R.id.lblconstMin);
-		   TextView lblMax = (TextView) findViewById(R.id.lblconstMax);
-		   //lblMin.setText(String.valueOf(constraint.getMinimumValue()));
-		   //lblMax.setText(String.valueOf(constraint.getMaximumValue()));
-
-		   //------------------get seekBar -------------------
-		   final SeekBar seekbar = (SeekBar) findViewById(R.id.editconstraintseekBar);
-
-		   if (constraintType.equals("float") || constraintType.equals("price")) {
-			   etValue.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
-			   divisionFactor = 100;
-		   } else {
-			   etValue.setInputType(InputType.TYPE_CLASS_NUMBER);
-			   divisionFactor = 1;
-		   }
-
-		   //seekbar.setMax(constraint.getMaximumValue().intValue() * divisionFactor);
-		   if (constraint.getValue() != null) {
-
-			   if (constraintType.equals("integer") || constraintType.equals("meter")) {
-				   divisionFactor = 1;
-				   // set seekbar progresslevel
-				   seekbar.setProgress(Integer.valueOf(constraint.getValue().toString()));
-			   }
-			   if (constraintType.equals("boolean")) {
-				   divisionFactor = 1;
-				   // set seekbar progresslevel
-				   int booleanValue = 0;
-				   if ((Boolean) constraint.getValue() == true) {
-					   booleanValue = 1;
-				   }
-				   seekbar.setProgress(booleanValue);
-			   }
-			   if (constraintType.equals("float") || constraintType.equals("price")) {
-				   divisionFactor = 100;
-				   // set seekbar progresslevel
-				   float tempFloat = Float.valueOf(constraint.getValue().toString());
-				   int tempInteger = (int) tempFloat;
-				   seekbar.setProgress(tempInteger * divisionFactor);
-			   }
-		   }
-		   seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-
-			   @Override
-			   public void onProgressChanged(SeekBar arg0, int arg1,
-											 boolean arg2) {
-				   if (constraintType.equals("float") || constraintType.equals("price")) {
-					   etValue.setText(String.valueOf((float) arg1 / divisionFactor));
-				   }
-
-				   if (constraintType.equals("integer") || constraintType.equals("meter")) {
-					   etValue.setText(String.valueOf(arg1 / divisionFactor));
-				   }
-
-				   if (constraintType.equals("boolean")) {
-					   if (arg1 == 0) {
-						   etValue.setText("false");
-					   } else {
-						   etValue.setText("true");
-					   }
-				   }
-
-			   }
-
-			   @Override
-			   public void onStartTrackingTouch(SeekBar arg0) {
-
-			   }
-
-			   @Override
-			   public void onStopTrackingTouch(SeekBar arg0) {
-
-			   }
-		   });*/
+		constraintView = constraint.createView(this);
+		constraintView.attach(getLayoutInflater(), placeholder);
 	}
 }
--- ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/UI/ConstraintViews/FloatConstraintView.java
+++ ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/UI/ConstraintViews/FloatConstraintView.java
@@ -1,16 +1,70 @@
 package de.uni.stuttgart.informatik.ToureNPlaner.UI.ConstraintViews;
 
 import android.content.Context;
+import android.text.InputType;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import de.uni.stuttgart.informatik.ToureNPlaner.Data.Constraints.Constraint;
+import de.uni.stuttgart.informatik.ToureNPlaner.Data.Constraints.FloatConstraint;
 import de.uni.stuttgart.informatik.ToureNPlaner.R;
 
 public class FloatConstraintView extends ConstraintView {
+	private static final int seekBarMax = 10000;
+	private final float divisionFactor;
+
 	public FloatConstraintView(Context context, Constraint constraint) {
 		super(context, constraint);
+		FloatConstraint type = (FloatConstraint) constraint.getType();
+		divisionFactor = (type.getMaximum() - type.getMinimum()) / (float) seekBarMax;
 	}
 
 	@Override
 	public int getLayout() {
 		return R.layout.float_constraint_layout;
+	}
+
+	@Override
+	protected void setup() {
+		TextView lblMin = (TextView) view.findViewById(R.id.lblconstMin);
+		TextView lblMax = (TextView) view.findViewById(R.id.lblconstMax);
+		final EditText etValue = (EditText) view.findViewById(R.id.txtconstValue);
+
+		final FloatConstraint type = (FloatConstraint) constraint.getType();
+
+		lblMin.setText(String.valueOf(type.getMinimum()));
+		lblMax.setText(String.valueOf(type.getMaximum()));
+
+		etValue.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+
+		//------------------get seekBar -------------------
+		final SeekBar seekbar = (SeekBar) view.findViewById(R.id.editconstraintseekBar);
+		seekbar.setMax(seekBarMax);
+		if (constraint.getValue() != null) {
+			float val = (Float) constraint.getValue();
+			seekbar.setProgress((int) (val * divisionFactor));
+		} else {
+			// TODO localize
+			etValue.setHint("Choose a value.");
+		}
+		seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+			@Override
+			public void onProgressChanged(SeekBar arg0, int arg1,
+			                              boolean arg2) {
+				float val = type.getMinimum() + (float) arg1 / divisionFactor;
+				constraint.setValue(val);
+				etValue.setText(Float.toString(val));
+			}
+
+			@Override
+			public void onStartTrackingTouch(SeekBar arg0) {
+
+			}
+
+			@Override
+			public void onStopTrackingTouch(SeekBar arg0) {
+
+			}
+		});
 	}
 }
