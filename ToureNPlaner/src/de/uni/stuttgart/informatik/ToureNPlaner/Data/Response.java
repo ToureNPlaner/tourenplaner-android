d2e2c343c810ebe898bf00a8dc1800d58d3033b2
Steffen Hanikel <steffen.hanikel@googlemail.com>
2012-02-14 22:07:38 +0100

    Merge fix

--- ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/Data/Response.java
+++ ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/Data/Response.java
@@ -1,20 +1,19 @@
 package de.uni.stuttgart.informatik.ToureNPlaner.Data;
 
+import de.uni.stuttgart.informatik.ToureNPlaner.Data.Constraints.ConstraintType;
+import org.codehaus.jackson.JsonNode;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 
-import org.codehaus.jackson.JsonNode;
-
-import de.uni.stuttgart.informatik.ToureNPlaner.Data.Constraints.ConstraintType;
-
-public class Response implements Serializable{
-/**
-	 * 
+public class Response implements Serializable {
+	/**
+	 *
 	 */
 	private static final long serialVersionUID = 1L;
 
-public static ArrayList<Node> parse(JsonNode object) {
-		
+	public static ArrayList<Node> parse(JsonNode object) {
+
 		ArrayList<Node> nodeList = new ArrayList<Node>();
 		JsonNode points = object.get("points");
 		for (JsonNode pointlist : points) {
@@ -22,15 +21,16 @@ public static ArrayList<Node> parse(JsonNode object) {
 		}
 		return nodeList;
 	}
-public static Node parsePoints(JsonNode object){
-	String name = object.get("name").asText();
-	int lt = object.get("lt").asInt();
-	int ln = object.get("ln").asInt();
-	
-ArrayList<ConstraintType> alct = new ArrayList<ConstraintType>();
-	Node node = new Node(name,lt,ln,alct);
-	return node;
-	
-}
+
+	public static Node parsePoints(JsonNode object) {
+		String name = object.get("name").asText();
+		int lt = object.get("lt").asInt();
+		int ln = object.get("ln").asInt();
+
+		ArrayList<ConstraintType> alct = new ArrayList<ConstraintType>();
+		Node node = new Node(0, name, lt, ln, alct);
+		return node;
+
+	}
 
 }
