f36dd18e29475c134690d9af44076864b7b4996b
Steffen Hanikel <hanikesn@studi.informatik.uni-stuttgart.de>
2012-02-15 15:29:33 +0100

    Fix SSL when no keystore is used

--- ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/ToureNPlanerApplication.java
+++ ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/ToureNPlanerApplication.java
@@ -22,12 +22,23 @@ public class ToureNPlanerApplication extends Application {
 	}
 
 	public static void setupSsl() {
+        TrustManagerFactory tmf;
 		try {
 			sslContext = SSLContext.getInstance("TLS");
-			keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            tmf = TrustManagerFactory.getInstance("X509");
+        } catch (Exception e) {
+            Log.e("TP", "SSL", e);
+            return;
+        }
+        try {
 			keyStore.load(ToureNPlanerApplication.context.openFileInput("keystore"), null);
-			TrustManagerFactory tmf = TrustManagerFactory.getInstance("X509");
-			tmf.init(keyStore);
+        } catch (Exception e) {
+            Log.e("TP", "SSL", e);
+            keyStore = null;
+        }
+        try {
+            tmf.init(keyStore);
 			sslContext.init(null, tmf.getTrustManagers(), null);
 		} catch (Exception e) {
 			Log.e("TP", "SSL", e);
