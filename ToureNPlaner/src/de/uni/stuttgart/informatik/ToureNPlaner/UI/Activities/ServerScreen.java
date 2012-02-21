6d8bc0ea6e33eac17f23d0b620bed2bb536ddacf
Steffen Hanikel <steffen.hanikel@googlemail.com>
2012-02-21 21:44:47 +0100

    Prevent multiple starts of application

--- ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/UI/Activities/ServerScreen.java
+++ ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/UI/Activities/ServerScreen.java
@@ -4,6 +4,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.*;
 import android.view.View.OnClickListener;
 import android.widget.*;
@@ -97,6 +98,9 @@ public class ServerScreen extends FragmentActivity implements Observer {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		checkForMultipleInstances();
+
 		setContentView(R.layout.serverscreen);
 
 		loadServerList();
@@ -105,6 +109,21 @@ public class ServerScreen extends FragmentActivity implements Observer {
 		setupButtons();
 
 		initializeHandler();
+	}
+
+	private void checkForMultipleInstances() {
+		// Possible work around for market launches. See http://code.google.com/p/android/issues/detail?id=2373
+		// for more details. Essentially, the market launches the main activity on top of other activities.
+		// we never want this to happen. Instead, we check if we are the root and if not, we finish.
+		if (!isTaskRoot()) {
+			final Intent intent = getIntent();
+			final String intentAction = intent.getAction();
+			if (intent.hasCategory(Intent.CATEGORY_LAUNCHER) && intentAction != null && intentAction.equals(Intent.ACTION_MAIN)) {
+				Log.w("TP", "Main Activity is not the root.  Finishing Main Activity instead of launching.");
+				finish();
+				return;
+			}
+		}
 	}
 
 	private void saveServerList() {
