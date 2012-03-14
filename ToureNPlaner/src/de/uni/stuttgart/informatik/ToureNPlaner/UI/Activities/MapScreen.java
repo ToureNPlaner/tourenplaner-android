153cc3c5c5371aad127b6a878683197cb8728083
Vivian Eggert <vivian.eggert@gmx.net>
2012-03-14 11:41:07 +0100

    removed 'back' on mapscreenmenu

--- ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/UI/Activities/MapScreen.java
+++ ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/UI/Activities/MapScreen.java
@@ -290,9 +290,6 @@ public class MapScreen extends MapActivity implements Session.Listener {
 				myIntent.putExtra(Session.IDENTIFIER, session);
 				startActivityForResult(myIntent, REQUEST_CONSTRAINTS);
 				return true;
-			case R.id.back:
-				finish();
-				return true;
 			case R.id.settings:
 				startActivity(new Intent(this, MapScreenPreferences.class));
 				return true;
