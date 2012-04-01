d0a911f9352de31016e6188d88713cdd72c9f7bf
Steffen Hanikel <steffen.hanikel@googlemail.com>
2012-04-01 23:49:36 +0200

    Moved ConstraintList into own Fragment

--- ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/UI/Activities/AlgorithmConstraintsScreen.java
+++ ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/UI/Activities/AlgorithmConstraintsScreen.java
@@ -4,72 +4,36 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.view.KeyEvent;
-import android.widget.FrameLayout;
-import android.widget.LinearLayout;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import de.uni.stuttgart.informatik.ToureNPlaner.Data.Constraints.Constraint;
-import de.uni.stuttgart.informatik.ToureNPlaner.R;
-import de.uni.stuttgart.informatik.ToureNPlaner.UI.ConstraintFragments.ConstraintFragment;
 
 import java.util.ArrayList;
 
 public class AlgorithmConstraintsScreen extends SherlockFragmentActivity {
 	public static String IDENTIFIER = "constraints";
 
-	private ArrayList<Constraint> constraints;
-
-	@Override
-	protected void onSaveInstanceState(Bundle outState) {
-		outState.putSerializable(IDENTIFIER, constraints);
-		super.onSaveInstanceState(outState);
-	}
-
 	@Override
 	@SuppressWarnings("unchecked")
-	public void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		// If we get created for the first time we get our data from the intent
-		Bundle data = savedInstanceState != null ? savedInstanceState : getIntent().getExtras();
-		constraints = (ArrayList<Constraint>) data.getSerializable(IDENTIFIER);
-
-		setContentView(R.layout.algorithmconstrainsscreen);
-
-		setupList();
-
-		if (savedInstanceState == null)
-			setupFragments();
-	}
-
-	private void setupFragments() {
-		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-		for (int i = 0; i < constraints.size(); i++) {
-			ft.add(i + 1, constraints.get(i).createFragment(i));
-		}
-		ft.commit();
-	}
-
-	private void setupList() {
-		LinearLayout list = (LinearLayout) findViewById(android.R.id.list);
-		for (int i = 0; i < constraints.size(); i++) {
-			FrameLayout frameLayout = new FrameLayout(this);
-			frameLayout.setId(i + 1);
-			list.addView(frameLayout);
+		if (savedInstanceState == null) {
+			FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+			ft.add(android.R.id.content, ConstraintListFragment.newInstance(
+					(ArrayList<Constraint>) getIntent().getExtras().getSerializable(IDENTIFIER)), "list");
+			ft.commit();
 		}
 	}
 
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 		if ((keyCode == KeyEvent.KEYCODE_BACK)) {
+			ConstraintListFragment fragment = ((ConstraintListFragment) getSupportFragmentManager().findFragmentByTag("list"));
+
 			Intent data = new Intent();
-			boolean dirty = false;
-			for (int i = 0; i < constraints.size(); i++) {
-				if (((ConstraintFragment) getSupportFragmentManager().findFragmentById(i + 1)).isDirty()) {
-					dirty = true;
-				}
-			}
+			boolean dirty = fragment.isDirty();
 			if (dirty)
-				data.putExtra(IDENTIFIER, constraints);
+				data.putExtra(IDENTIFIER, fragment.getConstraints());
 			setResult(dirty ? RESULT_OK : RESULT_CANCELED, data);
 		}
 		return super.onKeyDown(keyCode, event);
---
+++ ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/UI/Activities/ConstraintListFragment.java
@@ -0,0 +1,90 @@
+package de.uni.stuttgart.informatik.ToureNPlaner.UI.Activities;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import com.actionbarsherlock.app.SherlockFragment;
+import de.uni.stuttgart.informatik.ToureNPlaner.Data.Constraints.Constraint;
+import de.uni.stuttgart.informatik.ToureNPlaner.R;
+import de.uni.stuttgart.informatik.ToureNPlaner.UI.ConstraintFragments.ConstraintFragment;
+
+import java.util.ArrayList;
+
+public class ConstraintListFragment extends SherlockFragment {
+	public static String IDENTIFIER = "constraints";
+
+	private ArrayList<Constraint> constraints;
+
+	public boolean isDirty() {
+		for (int i = 0; i < constraints.size(); i++) {
+			if (((ConstraintFragment) getFragmentManager().findFragmentById(i + 1)).isDirty()) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		outState.putSerializable(IDENTIFIER, constraints);
+		super.onSaveInstanceState(outState);
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// If we get created for the first time we get our data from the intent
+		Bundle data = savedInstanceState != null ? savedInstanceState : getArguments();
+		constraints = (ArrayList<Constraint>) data.getSerializable(IDENTIFIER);
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+
+		setupList();
+
+		if (savedInstanceState == null)
+			setupFragments();
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.algorithmconstrainsscreen, null);
+	}
+
+	private void setupFragments() {
+		FragmentTransaction ft = getFragmentManager().beginTransaction();
+		for (int i = 0; i < constraints.size(); i++) {
+			ft.add(i + 1, constraints.get(i).createFragment(i));
+		}
+		ft.commit();
+	}
+
+	private void setupList() {
+		LinearLayout list = (LinearLayout) getView().findViewById(android.R.id.list);
+		for (int i = 0; i < constraints.size(); i++) {
+			FrameLayout frameLayout = new FrameLayout(getActivity());
+			frameLayout.setId(i + 1);
+			list.addView(frameLayout);
+		}
+	}
+
+	public static ConstraintListFragment newInstance(ArrayList<Constraint> constraints) {
+		ConstraintListFragment fragment = new ConstraintListFragment();
+		Bundle args = new Bundle();
+		args.putSerializable(IDENTIFIER, constraints);
+		fragment.setArguments(args);
+		return fragment;
+	}
+
+	public ArrayList<Constraint> getConstraints() {
+		return constraints;
+	}
+}
