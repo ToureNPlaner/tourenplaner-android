d698d707816a0b72a7c990641cb08d75081e55e3
Steffen Hanikel <steffen.hanikel@googlemail.com>
2012-04-12 17:55:57 +0200

    Updated Copyright

--- ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/Data/BillingItem.java
+++ ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/Data/BillingItem.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012 ToureNPlaner
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package de.uni.stuttgart.informatik.ToureNPlaner.Data;
 
 
@@ -9,7 +25,7 @@ import java.util.ArrayList;
 
 public class BillingItem implements Serializable {
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 1L;
 	private String algorithm;
@@ -22,7 +38,7 @@ public class BillingItem implements Serializable {
 	private String finishdate;
 	private int duration;
 	private String status;
-	
+
 	public String getAlgorithm() {
 		return algorithm;
 	}
@@ -62,23 +78,25 @@ public class BillingItem implements Serializable {
 	public String getStatus() {
 		return status;
 	}
-	public static BillingItem parseItem(JsonNode object){
+
+	public static BillingItem parseItem(JsonNode object) {
 		BillingItem billing = new BillingItem();
 		billing.algorithm = object.get("algorithm").asText();
 		billing.requestid = object.get("requestid").asInt();
 		billing.userid = object.get("userid").asInt();
-		billing.request ="null";//object.get("request").asText();
+		billing.request = "null";//object.get("request").asText();
 		billing.response = "null";//object.get("response").asText();
-		billing.cost= object.get("cost").asInt();
+		billing.cost = object.get("cost").asInt();
 		billing.requestdate = object.get("requestdate").asText();
-		billing.finishdate ="null";//object.get("finishdate").asText();
+		billing.finishdate = "null";//object.get("finishdate").asText();
 		billing.duration = object.get("duration").asInt();
 		billing.status = object.get("status").asText();
 		return billing;
-		
+
 	}
+
 	public static ArrayList<BillingItem> parse(JsonNode object) {
-		
+
 		ArrayList<BillingItem> billingItemlist = new ArrayList<BillingItem>();
 		JsonNode requests = object.get("requests");
 		for (JsonNode listItem : requests) {
--- ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/UI/Overlays/GpsDrawable.java
+++ ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/UI/Overlays/GpsDrawable.java
@@ -1,9 +1,28 @@
+/*
+ * Copyright 2012 ToureNPlaner
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package de.uni.stuttgart.informatik.ToureNPlaner.UI.Overlays;
 
-import android.graphics.*;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 
-public class GpsDrawable extends Drawable{
+public class GpsDrawable extends Drawable {
 	private final Paint paint;
 
 	public GpsDrawable(Paint paint) {
@@ -13,9 +32,9 @@ public class GpsDrawable extends Drawable{
 	@Override
 	public void draw(Canvas canvas) {
 		Rect bounds = this.getBounds();
-        //add a line for the circle
-        canvas.drawCircle(bounds.centerX(), bounds.centerY(), bounds.width()/2, paint);
-        }
+		//add a line for the circle
+		canvas.drawCircle(bounds.centerX(), bounds.centerY(), bounds.width() / 2, paint);
+	}
 
 	@Override
 	public void setAlpha(int i) {
