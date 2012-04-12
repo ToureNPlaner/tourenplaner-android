65d06ac070f61aa5b94c8efc2dfe1856e8526c1e
Steffen Hanikel <steffen.hanikel@googlemail.com>
2012-04-12 17:05:13 +0200

    Add time and dist ToPrev to ResultNode

--- ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/Data/ResultNode.java
+++ ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/Data/ResultNode.java
@@ -14,17 +14,21 @@ public class ResultNode implements Serializable {
 	private final String name;
 	private final String shortName;
 	private final HashMap<String, String> misc;
+	private final double distToPrev;
+	private final double timeToPrev;
 
-	public ResultNode(int id, GeoPoint geoPoint, String name, String shortName, HashMap<String, String> misc) {
+	public ResultNode(int id, GeoPoint geoPoint, String name, String shortName, HashMap<String, String> misc, double distToPrev, double timeToPrev) {
 		this.geoPoint = geoPoint;
 		this.id = id;
 		this.name = name;
 		this.shortName = shortName;
 		this.misc = misc;
+		this.distToPrev = distToPrev;
+		this.timeToPrev = timeToPrev;
 	}
 
-	public ResultNode(int id, int lt, int ln, String name, String shortName, HashMap<String, String> misc) {
-		this(id, new GeoPoint(lt / 10, ln / 10), name, shortName, misc);
+	public ResultNode(int id, int lt, int ln, String name, String shortName, HashMap<String, String> misc, double distToPrev, double timeToPrev) {
+		this(id, new GeoPoint(lt / 10, ln / 10), name, shortName, misc, distToPrev, timeToPrev);
 	}
 
 	public int getId() {
@@ -43,6 +47,14 @@ public class ResultNode implements Serializable {
 		return geoPoint;
 	}
 
+	public double getDistToPrev() {
+		return distToPrev;
+	}
+
+	public double getTimeToPrev() {
+		return timeToPrev;
+	}
+
 	public HashMap<String, String> getMisc() {
 		return misc;
 	}
@@ -53,6 +65,8 @@ public class ResultNode implements Serializable {
 		int id = node.path("id").asInt();
 		String name = node.path("name").asText();
 		String shortName = node.path("shortname").asText();
+		double distToPrev = node.path("distToPrev").asInt();
+		double timeToPrev = node.path("timeToPrev").asDouble();
 
 		HashMap<String, String> misc = new HashMap<String, String>();
 
@@ -63,11 +77,13 @@ public class ResultNode implements Serializable {
 					!n.getKey().equals("lt") &&
 					!n.getKey().equals("id") &&
 					!n.getKey().equals("name") &&
-					!n.getKey().equals("shortname")) {
+					!n.getKey().equals("shortname") &&
+					!n.getKey().equals("distToPrev") &&
+					!n.getKey().equals("timeToPrev")) {
 				misc.put(n.getKey(), n.getValue().asText());
 			}
 		}
 
-		return new ResultNode(id, lt, ln, name, shortName, misc);
+		return new ResultNode(id, lt, ln, name, shortName, misc, distToPrev, timeToPrev);
 	}
 }
