ad3b7927f3e97fde3ba77b3810eed225c49a8250
Christoph Haag <haagch.christoph@googlemail.com>
2013-09-13 18:53:38 +0200

    don't close sqlite db immediately...

--- ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/Data/LocalStorage/RoutesStorageDbHelper.java
+++ ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/Data/LocalStorage/RoutesStorageDbHelper.java
@@ -62,7 +62,6 @@ public class RoutesStorageDbHelper extends SQLiteOpenHelper {
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		db.execSQL(SQL_CREATE_ENTRIES);
-		db.close();
 	}
 
 	@Override
