55f3ba63a7ba92ca0f49a3cdd6e00e279eff0f3c
Niklas Schnelle <niklas@komani.de>
2013-10-08 15:02:44 +0200

    Make InfoScreen work with broken support lib

--- ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/UI/TabListener.java
+++ ToureNPlaner/src/de/uni/stuttgart/informatik/ToureNPlaner/UI/TabListener.java
@@ -39,35 +39,33 @@ public class TabListener<T extends Fragment> implements ActionBar.TabListener {
 		mActivity = activity;
 		mTag = tag;
 		mClass = clz;
+		mFragment = null;
 	}
 
 	/* The following are each of the ActionBar.TabListener callbacks */
 
 	public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
-		boolean shouldCommit = false;
-		if (ft == null) {
-			ft = mActivity.getSupportFragmentManager().beginTransaction();
-			shouldCommit = true;
-		}
-
 		mFragment = mActivity.getSupportFragmentManager().findFragmentByTag(mTag);
+		// We need a new transaction because ¯\_(ツ)_/¯
+		// see also https://groups.google.com/forum/#!topic/actionbarsherlock/ierFjBP-GuA
+		FragmentTransaction fft = mActivity.getSupportFragmentManager().beginTransaction();
 		// Check if the fragment is already initialized
 		if (mFragment == null) {
 			// If not, instantiate and add it to the activity
 			mFragment = Fragment.instantiate(mActivity, mClass.getName());
-			ft.add(android.R.id.content, mFragment, mTag);
+			fft.add(android.R.id.content, mFragment, mTag).commit();
 		} else {
 			// If it exists, simply attach it in order to show it
-			ft.attach(mFragment);
+			fft.attach(mFragment).commit();
 		}
-		if (shouldCommit)
-			ft.commit();
 	}
 
 	public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
 		if (mFragment != null) {
+			// We need a new transaction because ¯\_(ツ)_/¯
+			// see also https://groups.google.com/forum/#!topic/actionbarsherlock/ierFjBP-GuA
 			// Detach the fragment, because another one is being attached
-			ft.detach(mFragment);
+			mActivity.getSupportFragmentManager().beginTransaction().detach(mFragment).commit();
 		}
 	}
 
