472120628dcc7adbfe5a67aa3660a0a04fc90def
Christoph Haag <haagch.christoph@googlemail.com>
2014-05-27 18:47:58 +0200

    fix and update build: update build tools and dependencies and stuff remove workaround build script for some old gradle version fix missing translations (lint.xml errors are fatal build errors on assembleRelease) gradle howto is kind of useless now, but update and let it there anyway build release script updated

--- build_tourenplaner_release.sh
+++ build_tourenplaner_release.sh
@@ -9,16 +9,6 @@ START=$(dirname "${self}")
 
 cd "${START}"
 
-# create local.properties for the ToureNPlaner main application
-android update project -p ToureNPlaner
+(cd ToureNPlaner; gradle assembleRelease)
 
-# Actionbar source isn't part of the project right now.
-## create local.properties for the Actionbar library
-# android update project -n ActionBarSherlock -p JakeWharton-ActionBarSherlock/library
-
-### Shouldn't be needed with "-n ActionBarSherlock" above?
-## cp -a actionbar_build.xml JakeWharton-ActionBarSherlock/library/build.xml
-
-(cd ToureNPlaner; ant release)
-
-mv ToureNPlaner/bin/ToureNPlaner-release-unsigned.apk ./
+mv ToureNPlaner/build/apk/ToureNPlaner-release-unsigned.apk ./
